c18e1612e68da0f9cb3cf65675ff89978ba3b74b
Riley Rockwell <rileyrockwell@gmail.com>
2024-03-30 17:39:34 +0000

    yieldAllCombos

--- 1.2_decision_trees_dynamic_programming/yieldAllCombos.py
+++ 1.2_decision_trees_dynamic_programming/yieldAllCombos.py
@@ -1,3 +1,24 @@
+def powerSet(items):
+    N = len(items)
+
+    # enumerate the 2**N possible combinations
+    for i in range(2**N):
+        combo = []
+        for j in range(N):
+            # test bit jth of integer i
+            if (i >> j) % 2 == 1:
+                combo.append(items[j])
+
+        yield combo
+
+items = list(range(4))
+print(powerSet(items))
+
+x = [0, 1]
+for i in powerSet(x):
+    print(i)
+
+
 def yieldAllCombos(items):
     """
     Generates all combinations of N items into two bags, whereby each 
@@ -5,6 +26,19 @@ def yieldAllCombos(items):
 
     Yields a tuple, (bag1, bag2), where each bag is represented as 
     a list of which item(s) are in each bag.
+    
+    yields: a generator object?
     """
-    pass
+    
 
+
+x = [0, 1]
+y = [1, 0]
+print(yieldAllCombos(x))
+
+
+print("###")
+
+x = [0, 1]
+for i in powerSet(x):
+    print(i)
