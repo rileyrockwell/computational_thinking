650a17aab9b5391973341abb33e734f130940786
Riley Rockwell <riley.rockwell@gmail.com>
2024-04-28 10:00:52 -0700

    updates

--- problem_sets/2/ps2.py
+++ problem_sets/2/ps2.py
@@ -60,7 +60,7 @@ def load_map(map_filename):
 map_file = "/Users/riley/github/computational_thinking/ocw_template/problem_sets/2/mit_map.txt"
 print(load_map(map_file))
 
-# testing this sentence
+print(1)
 
 
 
